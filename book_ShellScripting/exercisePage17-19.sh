63415d72e65858d46cd9fb1ef56f344f75d2574a
antimo2001 <antimo2001@gmail.com>
2020-08-13 19:42:27 -0400

    add more exercises 3, 4 and new fnLogDebug

--- book_ShellScripting/exercisePage17-19.sh
+++ book_ShellScripting/exercisePage17-19.sh
@@ -1,14 +1,45 @@
 #!/bin/bash
+DEBUG="fn3:fn4"
 
-function f1 {
-  echo Shell Scripting is fun"!"
+function fnLogDebug {
+  RETVAL=99
+  if [ -n $1 ]; then
+    echo $DEBUG | grep -q "$1"
+    RETVAL=$?
+  fi
+  return $RETVAL
 }
 
-f1
-
-function f2 {
+function fn2 {
   M="Shell Scripting is fun!"
   echo $M
 }
 
-f2
+fnLogDebug "fn2" && echo "...Debug: run fn2" && fn2
+
+function fn3 {
+  H=$(hostname)
+  M="This script is running on ${H}"
+  echo $M
+}
+
+fnLogDebug "fn3" && echo "...Debug: run fn3" && fn3
+
+## -----------------------------------------------------------------------------
+## Check if /etc/shadow file exists and check if writable
+function fn4 {
+  SHADOWF=/etc/shadow
+  if [ -f $SHADOWF ]; then
+    echo "Shadow passwords are enabled"
+
+    if [ -w $SHADOWF ]; then
+      echo "You have permissions to edit $SHADOWF"
+    else
+      echo "You Do not have permissions to edit $SHADOWF"
+    fi
+  else
+    echo "Shadow file does NOT exist"
+  fi
+}
+
+fnLogDebug "fn4" && echo "...Debug: run fn4" && fn4
