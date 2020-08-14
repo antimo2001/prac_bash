1c5cb25f19d8e5dacc53ac1e3b0941ab8597b085
antimo2001 <antimo2001@gmail.com>
2020-08-14 00:01:20 -0400

    do more exercises 40,41

--- book_ShellScripting/exercisePage40-41.sh
+++ book_ShellScripting/exercisePage40-41.sh
@@ -11,10 +11,24 @@ function fnLogDebug {
   return $RETVAL
 }
 
+## -----------------------------------------------------------------------------
+## Display number of files in PWD
+function fnCountFiles {
+  local COUNTFILE1=$(ls | wc -l)
+  local COUNTFILE2=$(ls -l | tail --lines=+2 | wc -l)
+
+  echo "COUNTFILE1==$COUNTFILE1"
+  echo "COUNTFILE2==$COUNTFILE2"
+
+  if [ $COUNTFILE1 -eq $COUNTFILE2 ]; then
+    echo "counts are equal"
+  else
+    echo "counts are NOT equal!!"
+  fi
+}
+
 function fn1 {
-  local ERR_CODE=0
-  echo "This will exit with zero exit status"
-  return $ERR_CODE
+  fnCountFiles
 }
 
 function fn2 {
